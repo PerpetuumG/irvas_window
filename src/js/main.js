449b1bd50f10d662906d0d3d09b6321749ac7ecf
PerpetuumG <perpetuumg@gmail.com>
2022-11-21 22:54:58 +0200

    add Timer

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,11 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 addEventListener('DOMContentLoaded', () => {
   let modalState = {};
+  let deadline = '2022-12-01';
 
   changeModalState(modalState);
 
@@ -16,4 +18,5 @@ addEventListener('DOMContentLoaded', () => {
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
+  timer('.container1', deadline);
 });
